91a442018bd452286ba35b61f34ef5f273d81eab
KibeKelvin <kibekevlin@gmail.com>
2025-09-25 13:45:58 +0300

    Add files via upload

---
+++ SEM ONE UNITS PROGRAM.c
@@ -0,0 +1,25 @@
+//Simple c program(Hello World)single line comme
+
+/*
+NAME:KIBE KELVIN
+REG NO:CT101/G/24702/24
+DATE:15/9/2025
+*/
+
+#include<stdio.h>//preprocessor directive printf()
+//main function
+
+int main(){
+printf("Computer Science Year 1,Semester 1 25/26:\n");
+printf("SPC 2101:INTRODUCTION TO PROGRAMING AND PROBLEM SOLVING\n");
+printf("SPC 2102:INTRODUCTION TO COMPUTER ARCHITECTURE\n");
+printf("SPH 2101:PHYSICS\n");
+printf("SPH 2107:ANALOGUE ELCTRONICS\n");
+printf("SPM 2100:DISCRETE MATHEMATICS\n");
+printf("SPM 2104:MATHEMATICS FOR SCIENCE\n");
+printf("UCU 2101:COMMUNICATION SKILLS\n");
+printf("UCU 2105:FUNDAMENTALS OF ICT\n");
+
+return 0;
+}
+
